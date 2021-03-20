093bc22f196aa79be6c7fa29e6622dabb3599870
Alexandr <VeselovskiyAlexandr@mail.ru>
2021-03-20 11:05:43 +0300

    test 2

--- ConsoleApplication1.12.2/ConsoleApplication1.12.2.cpp
+++ ConsoleApplication1.12.2/ConsoleApplication1.12.2.cpp
@@ -7,68 +7,82 @@ using namespace std;
 int main()
 {
 	setlocale(LC_ALL, "Russian");
-	string numberOne, numberTwo;
-	int bools=0, cows=0, i, j, k=0;
+	string numberOne, numberTwo, str, s;
+	int bools = 0, cows = 0, i, j, k = 0;
 	bool correct;
 	cout << "\nВведите задуманное четырёхзначное число ";
 	cin >> numberOne;
-	
+
 	for (i = 0; i < 4; i++) {
-		if ((numberOne[i]< '0')|| (numberOne[i]> '9')|| (numberOne.length() != 4)) {
+		if ((numberOne[i] < '0') || (numberOne[i] > '9') || (numberOne.length() != 4)) {
 			correct = false;
 			cout << "\nЗадумайте правильно число. "; break;
 		}
 		else {
 			correct = true;
+
 		}
 	}
-	
-	while ((bools< 4)&&correct) {
+
+	while ((bools < 4) && correct) {
 		cout << "\nВведите второе четырёхзначное число ";
 		cin >> numberTwo;
 		for (j = 0; j < 4; j++) {
+
 			if ((numberTwo[j] < '0') || (numberTwo[j] > '9') || (numberTwo.length() != 4)) {
 				correct = false;
-				cout << "\nУкажите правильно правильно число. "; break;
+				cout << "\nУкажите правильно  число. "; break;
 			}
 			else {
 				correct = true;
+
 			}
 		}
+		str = numberOne; s = numberTwo;
 		if (correct) {
-			 int countCowsOne=0, countCowsTwo = 0;
+			int countCowsOne = 0, countCowsTwo = 0;
+			cows = 0; bools = 0;
 			for (i = 0; i < 4; i++) {
-				
-				for (k = i+1; k<numberOne.length(); k++) {
-					if (numberOne[i] == numberOne[k]) {
-						countCowsOne++; cout << countCowsOne << "\n";
-					}
-				}
-				    for (j = 0; j < 4; j++) {
-					    if (i == j && numberOne[i] == numberTwo[j]) {
+
+
+				for (j = 0; j < 4; j++) {
+
+					if((i==j)&&(numberOne[i] == numberTwo[j])) {
 						bools++;
-
 					}
-					
-					    if ((i != j) && (numberTwo[j] == numberOne[i]) && (countCowsTwo < 1)) {
-						cows++;	countCowsTwo++;
-						
+					else if ((i != j) && (numberTwo[j] == numberOne[i])) {
+						for (k = i; k < 4; k++) { 
+							if (str[k] == numberTwo[j]) {
+								countCowsOne++;
+	   					    }
+						}
+						for (k = j; k < 4; k++) {
+							if (s[k] == numberTwo[j]) {
+								countCowsTwo++;
+							}
+						}
+						if (((countCowsOne>1)|| (countCowsTwo>1))&&(countCowsOne!= countCowsTwo)){
+							
+						}
+						else {
+							cows++;
+						}
+						countCowsOne=0;  countCowsTwo = 0;
 					}
 				}
-			
-				if (bools == 4) {
-					cout << "\n             ПОЗДРАВЛЯЕМ!";
-					cout << "\n           Вы угадали число."; break;
+					if (bools == 4) {
+						cout << "\n             ПОЗДРАВЛЯЕМ!";
+						cout << "\n           Вы угадали число."; break;
+					}
+				}
 
+				if (bools < 4) {
+					cout << "\nВ Вашем числе " << bools << " Бык и " << cows << " корова.";
 				}
 			}
-	if (bools < 4) {
-cout << "\nВ Вашем числе " << bools << " Бык и " << cows- countCowsOne << " коров.";
-				}
-			
 		}
 	}
-	}
+
 
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
@@ -81,3 +95,4 @@ cout << "\nВ Вашем числе " << bools << " Бык и " << cows- countCo
 //   4. В окне "Список ошибок" можно просматривать ошибки.
 //   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
 //   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+	
